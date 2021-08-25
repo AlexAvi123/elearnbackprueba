6ad05694b42fbdc6fd37f5e3930d5ea649d0f8eb
Roly1313 <crolysteven2000@gmail.com>
2021-08-24 23:51:57 -0500

    Modificacion de UserController y de las rutas

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,158 +1,266 @@
-// const User = require("../models/UserModel");
-
-// class UserController {
-
-//   async saveUser(_user) {
-//       var user = new User(_user);
-//       await user.save();
-//   }
-//   deleteUser() {}
-//   changeUser() {}
-// }
-
-// module.exports = UserController;
-
-/*****************PARTE DE ROLY********************/
-
 const User = require("../models/UserModel");
 const bcrypt = require("bcrypt");
 const BCRYPT_SALT_ROUNDS = 12;
 
-const ObjectId = require("mongoose").Types.ObjectId;
+class UserController {
 
-//ESTE CODIGO ES PARA PODER INGRESAR UNA IMAGEN, ESTA COMENTADO POR SI DESPUES SE IMPLEMENTA ESO DE LA IMAGEN
-// const multer = require('multer');
-// const multerConfig = require('../utils/multerConfig');
-
-// const upload = multer (multerConfig).single("imagen");
-
-// exports.fileupload = async (req, res, next) => {
-//     upload(req, res, function(error){
-//         if(error){
-//             res.json({
-//                 message: error
-//             });
-//         }
-//         return next();
-//     });
-// };
-
-/*********************METODOS************************* */
-//Insertar usuario
-exports.saveUser = async (req, res) => {
-  const user = new User(req.body);
-  try {
-    //ESTE CODIGO ES PARA PODER INGRESAR UNA IMAGEN, ESTA COMENTADO POR SI DESPUES SE IMPLEMENTA ESO DE LA IMAGEN
-    // if(req.file && req.file.filename){
-    //     user.imagen=req.file.filename;
-    // }
-
-    var passwordHash = await bcrypt.hash(req.body.password, BCRYPT_SALT_ROUNDS);
-    user.password = passwordHash;
-
-    await user.save();
-    res.json({ message: "Nuevo Usuario agregado" });
-  } catch (error) {
-    if (error.code === 11000) {
-      res.status(400).json({
-        message: `Ya existe un Usuario con ese correo ${req.body.mail}`,
-      });
-    } else {
-      if (error.name == "ValidationError") {
-        //res.status(400).json(error.message);
-        res.json(error.message);
-      }
-    }
-    res.send(error);
-  }
-};
-
-// Listar usuarios
-exports.listUser = async (req, res) => {
-  try {
-    const user = await User.find({});
-    res.json(user);
-  } catch (error) {
-    res.send(error);
-  }
-};
-
-// Mostrar un usuario en especifico
-exports.showUser = async (req, res, next) => {
-  var user = null;
-
-  try {
+  async saveUser(_user) {
     try {
-      objectIdUser = ObjectId(req.params.id);
-      user = await User.findById(objectIdUser);
+      var user = new User(_user);
+      await user.save();
+      return { res: "ok" }
     } catch (error) {
-      user = await User.findOne({ mail: req.params.id });
+      return error
     }
-    if (!user) {
-      res.status(404).json({
-        message: "El Usuario no existe",
-      });
-    }
-    res.json(user);
-  } catch (error) {
-    res.status(400).json({
-      message: "Error al procesar la peticion",
-      err: error,
-    });
   }
-};
 
-// Actualizar un usuario
-exports.updateUser = async (req, res, next) => {
+
+  async findUser(_id, _mail) {
+    var user
+    try {
+      if(_id != null){
+        //objectIdUser = ObjectId(_id);
+        user = await User.findById(_id);
+      }else{
+        user = await User.findOne({ mail: _mail });
+      }
+    } catch (error) {
+    }
+    return user
+  }
+
+
+  async deleteUser(_id) { 
+    try {
+      await User.findOneAndDelete({ "_id": _id });
+      return { res: "ok" }
+    } catch (error) {
+      return error
+    }
+    
+  }
+  async changeUser() { }
+}
+
+//module.exports = UserController;
+
+
+/*********************METODOS PARA LAS RUTAS************************* */
+
+//Metodo para crear un usuario ruta POST /users/create
+exports.createUser = async (req, res) => {
+  var datos_user = req.body;
+  var user = new UserController();
   try {
-    let newUsuario = req.body;
-    //ESTE CODIGO ES PARA PODER INGRESAR UNA IMAGEN, ESTA COMENTADO POR SI DESPUES SE IMPLEMENTA ESO DE LA IMAGEN
-    // if(req.file && req.file.filename){
-    //     newUsuario.imagen=req.file.filename;
-    // }else{
-    //     const user = await User.findById(req.params.id);
-    //     newUsuario.imagen = user.imagen;
-    // }
-
-    if (req.body.password) {
-      var passwordHash = await bcrypt.hash(
-        req.body.password,
-        BCRYPT_SALT_ROUNDS
-      );
-      newUsuario.password = passwordHash;
+    //res.send(req.body._id);
+    var usuario_existe =  await user.findUser(req.body._id, req.body.mail);
+    if (!usuario_existe) {
+      //res.send("true");
+      //ESTE CODIGO ES PARA PODER INGRESAR UNA IMAGEN, ESTA COMENTADO POR SI DESPUES SE IMPLEMENTA ESO DE LA IMAGEN
+      // if(req.file && req.file.filename){
+      //     user.imagen=req.file.filename;
+      // }
+      if(req.body.password){
+        var passwordHash = await bcrypt.hash(req.body.password, BCRYPT_SALT_ROUNDS);
+        datos_user.password = passwordHash;
+      }
+      
+      var respuesta = await user.saveUser(datos_user);
+      //res.send(respuesta);
+      if (respuesta.res != "ok") {
+        if (respuesta.code === 11000) {
+          res.status(400).json({
+            message: `Ya existe un Usuario con ese correo ${req.body.mail}`,
+          });
+        } else {
+          if (respuesta.name == "ValidationError") {
+            res.json(respuesta.message);
+          }
+        }
+      } else {
+        res.json(respuesta);
+        //res.json({ message: "Nuevo Usuario agregado" });
+      }
+    }else{
+      res.json({"res": "User Exist"});
     }
-
-    const updateUsuario = await User.findOneAndUpdate(
-      { _id: req.params.id },
-      newUsuario,
-      { new: true }
-    );
-
-    res.json({
-      message: "Usuario Actualizado",
-    });
   } catch (error) {
-    if (error.code === 11000) {
-      res.status(400).json({
-        message: `Ya existe un Usuario con ese correo ${req.body.mail}`,
-      });
-    }
-    res.status(400).json({
-      message: "Error al procesar la peticion",
-    });
+    res.send(error);
   }
 };
 
-// Eliminar un usuario
+//Metodo para obtener un usuario ruta GET /users/find/:id
+exports.findUser = async (req, res) => {
+  var datos_user = req.body;
+  var user = new UserController();
+  res.json(await user.findUser(req.params.id));
+
+};
+
+// Elominar un usuario ruta DELETE /users/delete/:id
 exports.deleteUser = async (req, res, next) => {
   try {
-    await User.findOneAndDelete({ _id: req.params.id });
-    res.json({
-      message: "El Usuario Eliminado",
-    });
+    var user = new UserController();
+    var respuesta = await user.deleteUser(req.params.id);
+    //await User.findOneAndDelete({ _id: req.params.id });
+    if(respuesta.res == "ok"){
+      res.json({
+        message: "El Usuario Eliminado",
+      });
+    }else{
+      res.json(respuesta);
+    }
+    
   } catch (error) {
     res.status(400).json({
       message: "Error al procesar la peticion",
     });
   }
 };
+
+
+
+// /*****************PARTE DE ROLY********************/
+
+// const User = require("../models/UserModel");
+// const bcrypt = require("bcrypt");
+// const BCRYPT_SALT_ROUNDS = 12;
+
+// const ObjectId = require("mongoose").Types.ObjectId;
+
+// //ESTE CODIGO ES PARA PODER INGRESAR UNA IMAGEN, ESTA COMENTADO POR SI DESPUES SE IMPLEMENTA ESO DE LA IMAGEN
+// // const multer = require('multer');
+// // const multerConfig = require('../utils/multerConfig');
+
+// // const upload = multer (multerConfig).single("imagen");
+
+// // exports.fileupload = async (req, res, next) => {
+// //     upload(req, res, function(error){
+// //         if(error){
+// //             res.json({
+// //                 message: error
+// //             });
+// //         }
+// //         return next();
+// //     });
+// // };
+
+// /*********************METODOS************************* */
+// //Insertar usuario
+// exports.saveUser = async (req, res) => {
+//   const user = new User(req.body);
+//   try {
+//     //ESTE CODIGO ES PARA PODER INGRESAR UNA IMAGEN, ESTA COMENTADO POR SI DESPUES SE IMPLEMENTA ESO DE LA IMAGEN
+//     // if(req.file && req.file.filename){
+//     //     user.imagen=req.file.filename;
+//     // }
+
+//     var passwordHash = await bcrypt.hash(req.body.password, BCRYPT_SALT_ROUNDS);
+//     user.password = passwordHash;
+
+//     await user.save();
+//     res.json({ message: "Nuevo Usuario agregado" });
+//   } catch (error) {
+//     if (error.code === 11000) {
+//       res.status(400).json({
+//         message: `Ya existe un Usuario con ese correo ${req.body.mail}`,
+//       });
+//     } else {
+//       if (error.name == "ValidationError") {
+//         //res.status(400).json(error.message);
+//         res.json(error.message);
+//       }
+//     }
+//     res.send(error);
+//   }
+// };
+
+// // Listar usuarios
+// exports.listUser = async (req, res) => {
+//   try {
+//     const user = await User.find({});
+//     res.json(user);
+//   } catch (error) {
+//     res.send(error);
+//   }
+// };
+
+// // Mostrar un usuario en especifico
+// exports.showUser = async (req, res, next) => {
+//   var user = null;
+
+//   try {
+//     try {
+//       objectIdUser = ObjectId(req.params.id);
+//       user = await User.findById(objectIdUser);
+//     } catch (error) {
+//       user = await User.findOne({ mail: req.params.id });
+//     }
+//     if (!user) {
+//       res.status(404).json({
+//         message: "El Usuario no existe",
+//       });
+//     }
+//     res.json(user);
+//   } catch (error) {
+//     res.status(400).json({
+//       message: "Error al procesar la peticion",
+//       err: error,
+//     });
+//   }
+// };
+
+// // Actualizar un usuario
+// exports.updateUser = async (req, res, next) => {
+//   try {
+//     let newUsuario = req.body;
+//     //ESTE CODIGO ES PARA PODER INGRESAR UNA IMAGEN, ESTA COMENTADO POR SI DESPUES SE IMPLEMENTA ESO DE LA IMAGEN
+//     // if(req.file && req.file.filename){
+//     //     newUsuario.imagen=req.file.filename;
+//     // }else{
+//     //     const user = await User.findById(req.params.id);
+//     //     newUsuario.imagen = user.imagen;
+//     // }
+
+//     if (req.body.password) {
+//       var passwordHash = await bcrypt.hash(
+//         req.body.password,
+//         BCRYPT_SALT_ROUNDS
+//       );
+//       newUsuario.password = passwordHash;
+//     }
+
+//     const updateUsuario = await User.findOneAndUpdate(
+//       { _id: req.params.id },
+//       newUsuario,
+//       { new: true }
+//     );
+
+//     res.json({
+//       message: "Usuario Actualizado",
+//     });
+//   } catch (error) {
+//     if (error.code === 11000) {
+//       res.status(400).json({
+//         message: `Ya existe un Usuario con ese correo ${req.body.mail}`,
+//       });
+//     }
+//     res.status(400).json({
+//       message: "Error al procesar la peticion",
+//     });
+//   }
+// };
+
+// // Eliminar un usuario
+// exports.deleteUser = async (req, res, next) => {
+//   try {
+//     await User.findOneAndDelete({ _id: req.params.id });
+//     res.json({
+//       message: "El Usuario Eliminado",
+//     });
+//   } catch (error) {
+//     res.status(400).json({
+//       message: "Error al procesar la peticion",
+//     });
+//   }
+// };
--- src/routes/index.js
+++ src/routes/index.js
@@ -24,30 +24,84 @@
 /*****************PARTE DE ROLY********************/
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const BCRYPT_SALT_ROUNDS = 12;
 const UserController = require('../controller/UserController');
 const UnitController = require('../controller/UnitController');
 const TaskController = require('../controller/TaskController');
 
 
-module.exports = function(){
+module.exports = function () {
   /******************************** USUARIO ************************************************ */
-    router.post('/users' /*, UserController.fileupload*/ , UserController.saveUser); //agregar un usuario
-    router.get('/users', UserController.listUser); //listar todos los usuarios
-    router.get('/users/:id', UserController.showUser); //mostrar un usuario en especifico
-    router.put('/users/:id'/*, UserController.fileupload*/ , UserController.updateUser); //editar un usuario
-    router.delete('/users/:id', UserController.deleteUser);   //eliminar un usuario
+  //   router.post('/users' /*, UserController.fileupload*/ , UserController.saveUser); //agregar un usuario
+  //   router.get('/users', UserController.listUser); //listar todos los usuarios
+  //   router.get('/users/:id', UserController.showUser); //mostrar un usuario en especifico
+  //   router.put('/users/:id'/*, UserController.fileupload*/ , UserController.updateUser); //editar un usuario
+  //   router.delete('/users/:id', UserController.deleteUser);   //eliminar un usuario
 
-  /******************************** UNIDAD ************************************************ */
-    router.post('/unit', UnitController.saveUnit); //agregar una unidad
-    router.get('/unit', UnitController.listUnit); //listar todas las unidades
-    router.get('/unit/:id', UnitController.showUnit); //mostrar una unidad en especifico
-    router.put('/unit/:id', UnitController.updateUnit); //editar una unidad
-    router.delete('/unit/:id', UnitController.deleteUnit); //eliminar una unidad
+  // /******************************** UNIDAD ************************************************ */
+  //   router.post('/unit', UnitController.saveUnit); //agregar una unidad
+  //   router.get('/unit', UnitController.listUnit); //listar todas las unidades
+  //   router.get('/unit/:id', UnitController.showUnit); //mostrar una unidad en especifico
+  //   router.put('/unit/:id', UnitController.updateUnit); //editar una unidad
+  //   router.delete('/unit/:id', UnitController.deleteUnit); //eliminar una unidad
 
-  /******************************** LECCION ************************************************ */
-    router.post('/task', TaskController.saveTask); //agregar una leccion
-    router.get('/task', TaskController.prueba)
+  // /******************************** LECCION ************************************************ */
+  //   router.post('/task', TaskController.saveTask); //agregar una leccion
+  //   router.get('/task', TaskController.prueba)
 
-    return router;
+
+
+  /******************************** USUARIO ************************************************ */
+  router.post('/users/create' /*, UserController.fileupload*/ , UserController.createUser); //agregar un usuario
+ //estos metodos aun estan en pruebas
+  router.get('/users/find/:id' , UserController.findUser);
+  router.delete('/users/delete/:id', UserController.deleteUser); 
+
+
+
+  //Supongo que de esta forma es la que me decias que hiciera
+  // router.post("/users/create", async (req, res) => {
+  //   var datos_user = req.body;
+  //   var user = new UserController();
+  //   try {
+  //     //res.send(req.body._id);
+  //     var usuario_existe =  await user.findUser(req.body._id, req.body.mail);
+  //     if (!usuario_existe) {
+  //       //res.send("true");
+  //       //ESTE CODIGO ES PARA PODER INGRESAR UNA IMAGEN, ESTA COMENTADO POR SI DESPUES SE IMPLEMENTA ESO DE LA IMAGEN
+  //       // if(req.file && req.file.filename){
+  //       //     user.imagen=req.file.filename;
+  //       // }
+  //       if(req.body.password){
+  //         var passwordHash = await bcrypt.hash(req.body.password, BCRYPT_SALT_ROUNDS);
+  //         datos_user.password = passwordHash;
+  //       }
+        
+  //       var respuesta = await user.saveUser(datos_user);
+  //       //res.send(respuesta);
+  //       if (respuesta.res != "ok") {
+  //         if (respuesta.code === 11000) {
+  //           res.status(400).json({
+  //             message: `Ya existe un Usuario con ese correo ${req.body.mail}`,
+  //           });
+  //         } else {
+  //           if (respuesta.name == "ValidationError") {
+  //             res.json(respuesta.message);
+  //           }
+  //         }
+  //       } else {
+  //         res.json(respuesta);
+  //         //res.json({ message: "Nuevo Usuario agregado" });
+  //       }
+  //     }else{
+  //       res.json({"res": "User Exist"});
+  //     }
+  //   } catch (error) {
+  //     res.send(error);
+  //   }
+  // });
+
+  return router;
 
 }
