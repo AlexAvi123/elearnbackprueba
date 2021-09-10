e1b4815c37c44e1612699199ea5f9ffe8090c716
xaviermaster <xavier.velez3132@gmail.com>
2021-09-09 21:49:58 -0500

    g

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ const cors = require("cors");
 const routers = require("./routes");
 const app = express();
 
-app.set("port", process.env.PORT || 3000);
-app.use(express.static(__dirname + "/public"));
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
