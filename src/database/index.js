d7adf17082de09ff74cb4b1853a937c8ce49a0c2
Nicromano <ljose297@gmail.com>
2021-10-04 11:28:41 -0500

    env bdurl

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,15 @@
 
 /*****************PARTE DE ROLY********************/
 const mongoose = require('mongoose');
+require('dotenv').config()
+const BD_URL = process.env.BD_URL;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
     //'mongodb://localhost/LearningEnglish'
     //coneccion a la base de datos en la nube
-    'mongodb+srv://root:utm.english.cluster@englishcluster.eayiw.mongodb.net/LearningEnglish?retryWrites=true&w=majority',
+    //'mongodb+srv://root:utm.english.cluster@englishcluster.eayiw.mongodb.net/LearningEnglish?retryWrites=true&w=majority',
+    BD_URL,
     {
         useNewUrlParser : true,
         useUnifiedTopology: true,
